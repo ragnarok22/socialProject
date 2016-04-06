54de7a194a30736a448884020a79ec40474c923d
Reinier Hernández Ávila <rhernandeza@facinf.uho.edu.cu>
2016-04-06 18:23:56 -0400

    añadiendo las vistas para la privacidad, el acerca de y los terminos de uso

--- account/views.py
+++ account/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class LoginView(TemplateView):
     template_name = 'account/login.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'account/about.html'
+
+
+class TermsView(TemplateView):
+    template_name = 'account/terms.html'
+
+
+class PrivacyView(TemplateView):
+    template_name = 'account/privacy.html'
