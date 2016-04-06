8c7474b64fccd7048a46087579c4face0b44c5bc
Reinier Hernández Ávila <rhernandeza@facinf.uho.edu.cu>
2016-04-06 18:07:11 -0400

    creando la vista y la url para el login

--- account/views.py
+++ account/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+class LoginView(ListView):
+    template_name = 'account/login.html'
--- socialProject/urls.py
+++ socialProject/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from account.views import LoginView
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', LoginView.as_view(), name='login'),
 ]
