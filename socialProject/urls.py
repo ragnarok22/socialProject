2f46652be0f4602516bc951070a9c0bc62957591
Reinier Hernández Ávila <rhernandeza@facinf.uho.edu.cu>
2016-04-06 18:25:35 -0400

    añadiendo las urls para los terminos de uso, el acerca de y la privacidad

--- socialProject/urls.py
+++ socialProject/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from account.views import LoginView
+from account.views import LoginView, AboutView, TermsView, PrivacyView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', LoginView.as_view(), name='login'),
+    url(r'^', AboutView.as_view(), name='about'),
+    url(r'^', TermsView.as_view(), name='terms'),
+    url(r'^', PrivacyView.as_view(), name='privacy'),
 ]
