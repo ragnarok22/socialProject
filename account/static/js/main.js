ed56be2d35d837cf02aa6ee8aeccbfe4d32733c5
Reinier Hernández Ávila <rhernandeza@facinf.uho.edu.cu>
2016-04-07 01:19:24 -0400

    añadiendo peticiones ajax para about, pricacy y terms

--- account/static/js/main.js
+++ account/static/js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function () {
-    $.ajax('terms')
+    $('#terms').click(function (event) {
+        $('#spinner-terms').show(500);
+        $('#terms-content').load('terms', function (response, status, xhr) {
+            if(status == 'error'){
+                $('#terms-content').append('<i class="fa fa-times"><p>Se ha producido un error ' + xhr.status + '</p></i>');
+            }
+            $('#spinner-terms').hide(500);
+        });
+    });
+    $('#about').click(function (event) {
+        $('#spinner-about').show(500);
+        $('#about-content').load('about', function (response, status, xhr) {
+            if(status == 'error'){
+                $('#about-content').append('<i class="fa fa-times"><p>Se ha producido un error ' + xhr.status + '</p></i>');
+            }
+            $('#spinner-about').hide(500);
+        });
+    });
+    $('#privacy').click(function (event) {
+        $('#spinner-privacy').show(500);
+        $('#privacy-content').load('privacy', function (response, status, xhr) {
+            if(status == 'error'){
+                $('#privacy-content').append('<i class="fa fa-times"><p>Se ha producido un error ' + xhr.status + '</p></i>');
+            }
+            $('#spinner-privacy').hide(500);
+        });
+    });
 });
