7aed9d447b480b7efcb1ce332323df915e47fa20
Reinier Hernández Ávila <rhernandeza@facinf.uho.edu.cu>
2016-04-18 10:13:03 -0400

    mejorando mensaje de error a la hora de leer los terminos de servicios, el acerca y la privacidad

--- account/static/js/main.js
+++ account/static/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         $('#spinner-terms').show(500);
         $('#terms-content').load('terms', function (response, status, xhr) {
             if(status == 'error'){
-                $('#terms-content').append('<i class="fa fa-times"><p>Se ha producido un error ' + xhr.status + '</p></i>');
+                $('#terms-content').append('<p><i class="fa fa-times-circle-o"> Se ha producido un error ' + xhr.status + '</p></i>');
             }
             $('#spinner-terms').hide(500);
         });
@@ -12,7 +12,7 @@ $(document).ready(function () {
         $('#spinner-about').show(500);
         $('#about-content').load('about', function (response, status, xhr) {
             if(status == 'error'){
-                $('#about-content').append('<i class="fa fa-times"><p>Se ha producido un error ' + xhr.status + '</p></i>');
+                $('#about-content').append('<p><i class="fa fa-times-circle-o"> Se ha producido un error ' + xhr.status + '</p></i>');
             }
             $('#spinner-about').hide(500);
         });
@@ -21,7 +21,7 @@ $(document).ready(function () {
         $('#spinner-privacy').show(500);
         $('#privacy-content').load('privacy', function (response, status, xhr) {
             if(status == 'error'){
-                $('#privacy-content').append('<i class="fa fa-times"><p>Se ha producido un error ' + xhr.status + '</p></i>');
+                $('#privacy-content').append('<p><i class="fa fa-times-circle-o"> Se ha producido un error ' + xhr.status + '</p></i>');
             }
             $('#spinner-privacy').hide(500);
         });
