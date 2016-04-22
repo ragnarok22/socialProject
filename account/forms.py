2ad1337b3437169c290938345ccd05707357e479
Reinier Hernández Ávila <rhernandeza@facinf.uho.edu.cu>
2016-04-22 16:04:30 -0400

    arreglando la interfaz del login

--- account/forms.py
+++ account/forms.py
@@ -4,10 +4,11 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(
         label='Usuario',
-        required=True
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'validate', 'id': 'username'})
     )
     password = forms.CharField(
         label='Contraseña',
         required=True,
-        widget=forms.PasswordInput
+        widget=forms.PasswordInput(attrs={'class': 'validate', 'id': 'password'})
     )
